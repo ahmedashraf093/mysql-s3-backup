490f7745743a7b14a7efcfd061c89acdfe35da50
Ahmed Ashraf <ahmed.ashraf@cds-sw.com>
2026-03-15 14:08:54 +0200

    fix: handle unset S3_S3V4 parameter

--- src/run.sh
+++ src/run.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-if [ "$S3_S3V4" = "yes" ]; then
+if [ "${S3_S3V4:-no}" = "yes" ]; then
   aws configure set default.s3.signature_version s3v4
 fi
 
-if [ -z "$SCHEDULE" ]; then
+if [ -z "${SCHEDULE:-}" ]; then
   sh backup.sh
 else
   exec go-cron "$SCHEDULE" /bin/sh backup.sh
